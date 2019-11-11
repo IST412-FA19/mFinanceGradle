549b9e1eea23a8e950e664f4f9e0158f537f6b48
Sam <swhealon@gmail.com>
2019-11-11 17:25:13 -0600

    Reformatted the jumbotron to be smaller, updated SQL import statement

--- mFinance/src/main/resources/import.sql
+++ mFinance/src/main/resources/import.sql
@@ -5,7 +5,8 @@
 -- Author:  garre
 -- Created: Nov 3, 2019
 
-INSERT INTO `Application_User`(`id`, `username`, `password`) VALUES (0, 'admin', 'password');
+INSERT INTO `Application_User`(`id`,`address`, `address2`, `city`, `email`, `first_name`, `last_name`, `password`, `state`, `username`, `zip`) 
+VALUES (0, '123 Street','Unit101',  'New York',  'user@gmail.com', 'first', 'last', 'password', 'NY', 'admin', '10001');
 
 
 
