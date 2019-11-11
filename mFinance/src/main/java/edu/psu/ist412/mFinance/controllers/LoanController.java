de0ed474f8d5e803a84dbbdcbcd5cbe44a911cc1
Randi Semera <randisemera@gmail.com>
2019-11-11 14:34:09 -0500

    created carLoan.html and controller, linked to apply button in nav menu

---
+++ mFinance/src/main/java/edu/psu/ist412/mFinance/controllers/LoanController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.psu.ist412.mFinance.controllers;
+
+import edu.psu.ist412.mFinance.dao.ApplicationUserRepository;
+import edu.psu.ist412.mFinance.models.ApplicationUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Randi Semera
+ */
+//@RestController
+public class LoanController {
+//    @Autowired
+//    
+//    @RequestMapping(value = "/carLoan")
+//    public RedirectView loadCarView(){
+//        System.out.println("car Loan button selected");
+//        return new RedirectView("/carLoan");
+    }
+
